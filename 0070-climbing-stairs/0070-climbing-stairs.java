4ff1facc200b4385ef631f0aef64075ebd711d1e
parthasarathis26 <parthasarathis.22cse@kongu.edu>
2025-01-21 14:13:37 +0530

    Time: 0 ms (100%), Space: 40.2 MB (78.89%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,16 @@
 class Solution {
-    public int rec(int n,int dp[]){
+    
+    public int climbStairs(int n) {
         if(n==0||n==1||n==2){
             return n;
         }
-        if(dp[n]!=-1){
-            return dp[n];
+        int pr1=2;
+        int pr2=1;
+        for(int i=3;i<=n;i++){
+            int c=pr1+pr2;
+            pr2=pr1;
+            pr1=c;
         }
-        dp[n]=rec(n-1,dp)+rec(n-2,dp);
-        return dp[n];
-    }
-    public int climbStairs(int n) {
-        int dp[]=new int[n+1];
-        Arrays.fill(dp,-1);
-        return rec(n,dp);
+        return pr1;
     }
 }
