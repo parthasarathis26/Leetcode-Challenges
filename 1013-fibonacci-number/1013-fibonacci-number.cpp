5a8c5ed834ce18d85e541fa0be9395951e8475f5
parthasarathis26 <parthasarathis.22cse@kongu.edu>
2025-01-21 13:26:01 +0530

    Time: 0 ms (100%), Space: 7.7 MB (81.54%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -4,6 +4,13 @@ public:
         if(n==0||n==1){
             return n;
         }
-        return fib(n-1)+fib(n-2);
+        int pr1=1;
+        int pr2=0;
+        for(int i=2;i<=n;i++){
+            int c=pr1+pr2;
+            pr2=pr1;
+            pr1=c;
+        }
+        return pr1;
     }
 };
