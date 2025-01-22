b6fce7da7896bfd5e5002e3298175c3e5bb43bdb
parthasarathis26 <parthasarathis.22cse@kongu.edu>
2025-01-22 10:06:41 +0530

    Time: 0 ms (100%), Space: 40.9 MB (16.91%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solve(int i,int j,int m,int n,int dp[][]){
+        if(i>=m||j>=n){
+            return 0;
+        }
+        if(i==m-1&&j==n-1){
+            return 1;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        dp[i][j]=solve(i+1,j,m,n,dp)+solve(i,j+1,m,n,dp);
+        return dp[i][j];
+    }
+    public int uniquePaths(int m, int n) {
+        int dp[][]=new int[m][n];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        return solve(0,0,m,n,dp);
+    }
+}
