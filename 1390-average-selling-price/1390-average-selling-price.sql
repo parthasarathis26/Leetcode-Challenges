205bb60886c11b7ecaedcf60bc7f7684c26351d9
parthasarathis26 <parthasarathis.22cse@kongu.edu>
2025-07-19 22:27:52 +0530

    Time: 959 ms (15.74%), Space: 0B (100%) - LeetHub

--- 1390-average-selling-price/1390-average-selling-price.sql
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -1,5 +1,5 @@
 SELECT p.product_id,
-        ROUND(IFNULL(sum(u.units*p.price)/sum(u.units),0),2)As average_price
-        FROM Prices p Left Join UnitsSold u on p.product_id=u.product_id AND u.purchase_date Between p.start_date and p.end_date
-        Group By p.product_id
+ROUND(IFNULL(sum(u.units*p.price)/sum(u.units),0),2)As average_price
+FROM Prices p Left Join UnitsSold u on p.product_id=u.product_id AND u.purchase_date Between p.start_date and p.end_date
+Group By p.product_id
 
