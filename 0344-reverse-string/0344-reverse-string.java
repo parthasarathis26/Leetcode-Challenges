ecf9cc3925ed5c5cf0f032876537eade2b60f1f5
parthasarathis26 <parthasarathis.22cse@kongu.edu>
2025-09-14 18:17:33 +0530

    Time: 121 ms (4.4%), Space: 46.7 MB (75.71%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int left=0;
+        int right=s.length-1;
+        while(left<right){
+            char c=s[left];
+            s[left]=s[right];
+            s[right]=c;
+            left++;
+            right--;
+            
+        }
+        for(int i=0;i<s.length;i++){
+            System.out.print(s[i]);
+        }
+    }
+}
